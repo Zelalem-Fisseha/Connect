5a09efb42d6b53ff19e4361ca1eb6312dd2ce963
zelalem-fisseha <zelalem.fisseha@bitscollege.edu.et>
2025-08-02 00:07:02 +0300

    This is all the end points done

--- BackEnd/app/controllers/applications_controller.rb
+++ BackEnd/app/controllers/applications_controller.rb
@@ -1,2 +1,28 @@
 class ApplicationsController < ApplicationController
+  before_action :set_job_post
+  
+  def index
+     @applications = @job_post.applications.includes(:job_seeker_profile)
+    render json: @applications.as_json(include: { job_seeker_profile: { only: :title } })
+  end
+  
+  def create
+    @application = @job_post.applications.build(application_params)
+    if @application.save
+      render json: @application, status: :created
+    else
+      render json: @application.errors, status: :unprocessable_entity
+    end
+  end
+  private
+  def set_job_post
+    @job_post = JobPost.find(params[:job_post_id])
+  end
+  def application_params
+    params.require(:application).permit(
+      :job_seeker_profile_id,
+      :cover_letter,
+      :status
+    )
+  end
 end
--- BackEnd/app/controllers/employer_profiles_controller.rb
+++ BackEnd/app/controllers/employer_profiles_controller.rb
@@ -1,2 +1,55 @@
 class EmployerProfilesController < ApplicationController
+  before_action :set_user
+  def show
+    @employer_profile = @user.employer_profile
+
+    if @employer_profile
+      render json: @employer_profile
+    else
+      render json: { error: 'Employer profile not found' }, status: :not_found
+    end
+  end
+  def create
+    @employer_profile = @user.build_employer_profile(employer_profile_params)
+
+    if @employer_profile.save
+      render json: @employer_profile,
+       status: :created,
+       location: user_employer_profile_url(@user)
+
+    else
+      render json: @employer_profile.errors, status: :unprocessable_entity
+    end
+  end
+  def update
+    @employer_profile = @user.employer_profile
+
+    if @employer_profile.update(employer_profile_params)
+      render json: @employer_profile
+    else
+      render json: @employer_profile.errors, status: :unprocessable_entity
+    end
+  end
+  def destroy
+    @employer_profile = @user.employer_profile
+
+    if @employer_profile
+      @employer_profile.destroy
+      head :no_content
+    else
+      render json: { error: 'Employer profile not found' }, status: :not_found
+    end
+  end
+  private
+  def set_user
+    @user = User.find(params[:user_id])
+  end
+  def employer_profile_params
+    params.require(:employer_profile).permit(
+      :company_name,
+      :company_description,
+      :industry,
+      :location,
+    )
+  end
 end
